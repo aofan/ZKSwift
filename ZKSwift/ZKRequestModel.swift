f6dfdf6929197dffdf9475d8b9de8c85055f5190
lizhikai <zhikai.li@jrj.com.cn>
2016-07-08 18:28:04 +0800

    增加上下拉刷新

---
+++ ZKSwift/ZKRequestModel.swift
@@ -0,0 +1,13 @@
+//
+//  ZKRequestModel.swift
+//  ZKSwift
+//
+//  Created by lizhikai on 16/7/8.
+//  Copyright © 2016年 ZK. All rights reserved.
+//
+
+import UIKit
+
+class ZKRequestModel: NSObject {
+
+}
