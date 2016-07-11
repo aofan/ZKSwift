536874c155d5dbde126e0cb6980fa9535a96149e
aofan <673875394@>
2016-07-11 21:16:03 +0800

    增加source

---
+++ source/ZKBaseViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ZKBaseViewController.swift
+//  ZKSwift
+//
+//  Created by lizhikai on 16/7/7.
+//  Copyright © 2016年 ZK. All rights reserved.
+//
+
+import UIKit
+
+class ZKBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
